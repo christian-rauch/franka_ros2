3f68039b6f04b0127e189d2a503131c1b4ca1cb6
Christian Rauch <Christian.Rauch@unileoben.ac.at>
2026-09-10 12:47:15 +0200

    turn Franka state broadcaster error into debug message

--- franka_robot_state_broadcaster/src/franka_robot_state_broadcaster.cpp
+++ franka_robot_state_broadcaster/src/franka_robot_state_broadcaster.cpp
@@ -111,7 +111,7 @@ controller_interface::return_type FrankaRobotStateBroadcaster::update(
 
     // publish state
     if (!realtime_franka_state_publisher->try_publish(msg_state)) {
-      RCLCPP_ERROR(get_node()->get_logger(), "Cannot publish franka state.");
+      RCLCPP_DEBUG(get_node()->get_logger(), "Cannot publish franka state.");
     }
   }
 
